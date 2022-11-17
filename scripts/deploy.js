ae79b22dfc8635f5fd260d6e5105548fe5c310ae
Naman Bhatia <namanbhatia11111@gmail.com>
2022-11-17 23:42:22 +0530

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+async function main() {
+  const MyNFT = await ethers.getContractFactory("MyNFT");
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const myNFT = await MyNFT.deploy();
+  console.log("Contract deployed to address:", myNFT.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+//0x9D2fD20536e9ADC6d13bc95E3DEdFD5D7b09f77B
